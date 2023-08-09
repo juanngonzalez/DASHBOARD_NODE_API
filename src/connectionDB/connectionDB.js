2b3ad6a4a1b33176ae8f837dc0ccc18cc748ab61
Juan Gabriel Gonzalez <juan.g.gonzalez13400@gmail.com>
2023-08-09 16:33:22 -0300

    changes

--- src/connectionDB/connectionDB.js
+++ src/connectionDB/connectionDB.js
@@ -1,24 +1,45 @@
+// import { Sequelize } from "sequelize";
+// import "dotenv/config.js"
+
+// const database=process.env.DATABASE
+// const username=process.env.DB_USERNAME
+// const password=process.env.DB_PASSWORD
+// const host=process.env.DB_HOST
+// const port=process.env.DB_PORT
+// const dialect=process.env.DB_DIALECT
+
+// const connectionDB = new Sequelize(database, username, password, {
+//   host,
+//   port,
+//   dialect,
+// });
+
+// try {
+//   await connectionDB.authenticate();
+//   console.log("Connection has been established successfully.");
+// } catch (error) {
+//   console.error("Unable to connect to the database:", error);
+// }
+
+// export default connectionDB
+
 import { Sequelize } from "sequelize";
-import "dotenv/config.js"
+import "dotenv/config.js";
 
-const database=process.env.DATABASE
-const username=process.env.DB_USERNAME
-const password=process.env.DB_PASSWORD
-const host=process.env.DB_HOST
-const port=process.env.DB_PORT
-const dialect=process.env.DB_DIALECT
+const databaseUrl = process.env.DATABASE_URL;
 
-const connectionDB = new Sequelize(database, username, password, {
-  host,
-  port,
-  dialect,
+const connectionDB = new Sequelize(databaseUrl, {
+  // Otras opciones de configuración de Sequelize
 });
 
-try {
-  await connectionDB.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
+// Puedes envolver el código en una función async para usar await.
+async function connectToDatabase() {
+  try {
+    await connectionDB.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
-export default connectionDB
+export default connectionDB;
